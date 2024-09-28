ced7f6162a1fc173f48c627bd79a73225386191b
sonya1045 <smikolaenko7@gmail.com>
2024-09-28 19:30:07 +0300

    update

--- hotel/settings.py
+++ hotel/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
