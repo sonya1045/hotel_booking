7d9fd35e60cd7a22ed76ba6e2c9190a6f2c41182
sonya1045 <smikolaenko7@gmail.com>
2024-10-16 18:19:46 +0300

    update

--- auth_system/views.py
+++ auth_system/views.py
@@ -20,14 +20,16 @@ def login_views(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data = request.POST)
         if form.is_valid():
+
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
             user = authenticate(request, username = username, password = password)
             if user is not None:
-                login(request, user)
+                login(request, user) 
                 return redirect("room-list")
             else: 
                 messages.error(request, message="Incorrect username")
+
     else:
         form = AuthenticationForm()
         context = {
