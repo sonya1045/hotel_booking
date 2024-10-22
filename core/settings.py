4e7fc8945d41a5f65f2641ac0ee5ce8336318766
sonya1045 <smikolaenko7@gmail.com>
2024-10-22 18:05:51 +0300

    update

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_booking',
     'auth_system',
+    'app_booking',
+    
 ]
 
 MIDDLEWARE = [
